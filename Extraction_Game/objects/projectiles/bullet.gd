0d39e8bfe27d0ac603b36d85125d42c9b996daf7
Jeffrey Freet <55461929+LethalInjectionStudios@users.noreply.github.com>
2024-01-01 10:42:47 -0800

    removed buggy code from main

--- Extraction_Game/objects/projectiles/bullet.gd
+++ Extraction_Game/objects/projectiles/bullet.gd
@@ -4,22 +4,13 @@ extends Area2D
 var damage: int = 50
 var armor_penetration: int = 0
 var speed: int = 500
-var range: int = 250
 var recoil: int
 var direction: Vector2 = Vector2.UP
 var owner_actor
 
-var _distance_traveled: float
 
 func _process(delta):
 	position += direction * speed * delta
-	_distance_traveled = position.distance_to(owner_actor.position)
-	
-	if _distance_traveled > range:
-		damage = damage / 2
-		
-	if _distance_traveled > range * 2:
-		queue_free()
 
 
 func _on_body_entered(body):
