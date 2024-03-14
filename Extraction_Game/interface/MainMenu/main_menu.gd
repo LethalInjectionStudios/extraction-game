48d867cb12a6657c113cc826f1509ab9e00cc295
Jeffrey Freet <55461929+LethalInjectionStudios@users.noreply.github.com>
2024-03-13 22:16:31 -0700

    22-MainMenu

--- Extraction_Game/interface/MainMenu/main_menu.gd
+++ Extraction_Game/interface/MainMenu/main_menu.gd
@@ -4,5 +4,25 @@ extends Control
 @export var play_button: Button
 @export var scene: PackedScene
 
-func _on_button_pressed() -> void:
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(scene)
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_twitter_pressed() -> void:
+	OS.shell_open("https://twitter.com/LethalInjStudio")
+
+
+func _on_twitch_pressed() -> void:
+	OS.shell_open("https://www.twitch.tv/lethalinjectiongames")
+
+
+func _on_discord_pressed() -> void:
+	OS.shell_open("https://discord.gg/9pHs8dBdp3")
