eb8dfc76b5c3ad91583b903681f922d60eaa5673
Jeffrey Freet <55461929+LethalInjectionStudios@users.noreply.github.com>
2024-04-19 16:08:38 -0700

    25-BuildForestLevel

--- Extraction_Game/resources/item.gd
+++ Extraction_Game/resources/item.gd
@@ -1,10 +1,23 @@
 class_name Item
 extends Resource
 
+## Name of the item
 @export var name: String
+
+## Type of item. See  [code]Globals.ItemType[/code]  for available values
 @export var type: Globals.Item_Type
+
+## Path to the sprite for the item
 @export var sprite: String
+
+## Monetary value of the item
 @export var value: int
+
+## Weight of the item in the inventory
 @export var weight: float
+
+## If [code]true[/code] the item can be stacked in the inventory
 @export var is_stackable: bool
+
+## How large a stack of items can be
 @export var stack_size: int
